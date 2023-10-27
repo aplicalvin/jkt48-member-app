a2cb8b858e2e06ec0b06f54d97197c3766c56ad2
calvins-simbolon <thecalvins.simbolon@gmail.com>
2023-10-27 22:54:36 +0700

    MemberList Page #1 - OverAll Structure is Done

--- lib/screens/memberList.dart
+++ lib/screens/memberList.dart
@@ -15,14 +15,43 @@ class MemberList extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Padding(
-            padding: const EdgeInsets.only(right: 12.0, left: 12.0, top: 24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MemberListCard(), // Menggunakan widget MemberListCard
-              ],
-            ),
-          ),
+              padding:
+                  const EdgeInsets.only(right: 12.0, left: 12.0, top: 24.0),
+              child: Center(
+                child: Wrap(
+                  alignment: WrapAlignment
+                      .start, // Menentukan alignment (start, end, center, dll.)
+                  spacing: 16.0, // Jarak horizontal antara widget
+                  runSpacing: 16.0, // Jarak vertikal antara widget
+                  children: <Widget>[
+                    MemberListCard(
+                      linkGambarMember:
+                          'https://jkt48.com/profile/adzana_shaliha.jpg',
+                      namaMember: 'Adzana Shaliha',
+                      teksGenerasi: 'Generasi 8',
+                    ),
+                    MemberListCard(
+                      linkGambarMember:
+                          'https://jkt48.com/profile/flora_shafiq.jpg',
+                      namaMember: 'Flora Shafiq',
+                      teksGenerasi: 'Generasi 7',
+                    ),
+                    MemberListCard(
+                      linkGambarMember:
+                          'https://jkt48.com/profile/febriola_sinambela.jpg',
+                      namaMember: 'Febriola Sinambela',
+                      teksGenerasi: 'Generasi 7',
+                    ),
+                    MemberListCard(
+                      linkGambarMember:
+                          'https://jkt48.com/profile/azizi_asadel.jpg',
+                      namaMember: 'Azizi Asadel',
+                      teksGenerasi: 'Generasi 7',
+                    ),
+                    // Tambahkan MemberListCard lainnya di sini
+                  ],
+                ),
+              )),
         ),
       ),
     );
--- lib/widgets/MemberList/MemberListCard.dart
+++ lib/widgets/MemberList/MemberListCard.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 
 class MemberListCard extends StatelessWidget {
-  const MemberListCard({Key? key}) : super(key: key);
+  final String linkGambarMember;
+  final String namaMember;
+  final String teksGenerasi;
+
+  MemberListCard({
+    required this.linkGambarMember,
+    required this.namaMember,
+    required this.teksGenerasi,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           // Gambar di atas
           Image.network(
-            'https://jkt48.com/profile/flora_shafiq.jpg',
-            width: 100, // Sesuaikan lebar sesuai kebutuhan
-            height: 100, // Sesuaikan tinggi sesuai kebutuhan
+            linkGambarMember,
+            width: 150,
+            height: 150,
           ),
+          SizedBox(height: 8.0),
 
-          // Teks pertama di bawah gambar
+          // Teks pertama di bawah gambar (namaMember)
           Text(
-            'Teks Pertama',
+            namaMember,
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
 
-          // Teks kedua di bawah gambar
+          // Teks kedua di bawah gambar (teksGenerasi)
           Text(
-            'Teks Kedua',
+            teksGenerasi,
             style: TextStyle(fontSize: 14, color: Colors.grey),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
